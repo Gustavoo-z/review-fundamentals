05e2ad750918c6c3422c009eb2097bcc57cd675b
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-02-27 17:09:41 -0300

    Add arrays/2

---
+++ Arrays/array-de-arrays.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+console.log(`A aluna ${alunos[1]} tirou a nota ${medias[1]}`);
